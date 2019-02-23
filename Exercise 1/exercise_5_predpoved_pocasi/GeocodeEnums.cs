dd83f94e72c9d1a8a2d1c431958f5c790052ea04
pavelkrivda <st52934@student.upce.cz>
2019-02-23 22:12:40 +0100

    Přidání geolokace. Stačí zadat místo a dohledají se souřadnice a následně dohledání teplot.

---
+++ Exercise 1/exercise_5_predpoved_pocasi/GeocodeEnums.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_5_predpoved_pocasi
+{
+    public enum LangType : byte { ru_RU, uk_UA, be_BY, en_US, en_BR, tr_TR }
+
+    public enum KindType : byte { house, street, metro, district, locality }
+
+    public enum ScoType : byte { longlat, latlong }
+}
--- Exercise 1/exercise_5_predpoved_pocasi/Program.cs
+++ Exercise 1/exercise_5_predpoved_pocasi/Program.cs
@@ -1,60 +1,71 @@
-﻿using System;
+﻿using exercis_5_predpoved_pocasi;
+using exercise_5_predpoved_pocasi;
+using exercise_5_predpoved_pocasi.Yandex;
+using System;
 using System.Collections.Generic;
-using System.Diagnostics.PerformanceData;
+using System.IO;
 using System.Net;
-using System.Runtime.InteropServices;
-using System.Runtime.Serialization.Formatters;
-using System.Text;
 using System.Xml;
 
 namespace exercis_1_5_predpoved_pocasi
 {
     class Program
     {
+        private static List<string> teploty = new List<string>();
+        private static List<string> rychlostVetru = new List<string>();
+        private static List<string> tlak = new List<string>();
+        private static List<string> cas = new List<string>();
+
         public static void Main(string[] args)
         {
             WebClient client = new WebClient();
             XmlDocument xmlDocument = new XmlDocument();
 
+            //string result =
+            //    client.DownloadString("https://api.met.no/weatherapi/locationforecast/1.9/?lat=60.10&lon=9.58");
+
+            Console.Write("Zadej místo pro které se zobrazí informace o počasí: ");
+            GeoObjectCollection results = YandexGeocoder.Geocode(Console.ReadLine());
+            string lat, lon;
+            lat = results[0].Point.Lat.ToString().Replace(',', '.');
+            lon = results[0].Point.Long.ToString().Replace(',', '.');
+            Console.WriteLine($"Souřadnice zadaného místa.\nZeměpisná šířka: {lat}  zeměpisná délka: {lon}");
+
             string result =
-                client.DownloadString("https://api.met.no/weatherapi/locationforecast/1.9/?lat=60.10&lon=9.58");
-
-
-//            double lat = 60.10, lon = 9.58;
-//            string result =
-//                client.DownloadString($"https://api.met.no/weatherapi/locationforecast/1.9/?lat={lat}&lon={lon}");
+                client.DownloadString(
+                    string.Format("https://api.met.no/weatherapi/locationforecast/1.9/?lat={0}&lon={1}", lat, lon));
 
             xmlDocument.LoadXml(result);
 
-//            Console.WriteLine("Po nodech");
-            // Po nodech dle nazvu
-//            List<string> teploty = parserXML(xmlDocument, "temperature", "value");
-//            List<string> rychlostVetru = parserXML(xmlDocument, "windSpeed", "mps");
-//            List<string> tlak = parserXML(xmlDocument, "pressure", "value");
-//            List<string> cas = new List<string>();
-//
-//            for (int i = 0; i < tlak.Count; i++)
-//            {
-//                Console.WriteLine($"{teploty[i]}°C\t {rychlostVetru[i]}m/s\t {tlak[i]}hPa");
-//            }
-
-            // Rekurzivně 
-            List<string> teploty = new List<string>();
-            List<string> rychlostVetru = new List<string>();
-            List<string> tlak = new List<string>();
-            List<string> cas = new List<string>();
+            //Rekurzivně
             Console.WriteLine("Rekurzivně");
             XmlNode hlavniNod = xmlDocument.DocumentElement;
-            ProjdiNody(teploty, rychlostVetru, tlak, cas, hlavniNod);
+            ProjdiNody(hlavniNod);
 
             for (int i = 0; i < tlak.Count; i++)
             {
                 Console.WriteLine($"{cas[i]}\t {teploty[i]}°C\t {rychlostVetru[i]}m/s\t {tlak[i]}hPa");
             }
+
+
+
+
+            //Console.WriteLine("Po nodech");
+            //Po nodech dle nazvu
+            //List<string> teploty = parserXML(xmlDocument, "temperature", "value");
+            //List<string> rychlostVetru = parserXML(xmlDocument, "windSpeed", "mps");
+            //List<string> tlak = parserXML(xmlDocument, "pressure", "value");
+            //List<string> cas = new List<string>();
+
+            //for (int i = 0; i < tlak.Count; i++)
+            //{
+            //    Console.WriteLine($"{teploty[i]}°C\t {rychlostVetru[i]}m/s\t {tlak[i]}hPa");
+            //}
+
+
         }
 
-        private static void ProjdiNody(List<string> teploty, List<string> rychlostVetru, List<string> tlak,
-            List<string> cas, XmlNode hlavniNod)
+        private static void ProjdiNody(XmlNode hlavniNod)
         {
             foreach (XmlNode podNod in hlavniNod.ChildNodes)
             {
@@ -91,22 +102,22 @@ namespace exercis_1_5_predpoved_pocasi
                     }
                 }
 
-                ProjdiNody(teploty, rychlostVetru, tlak, cas, podNod);
+                ProjdiNody(podNod); 
             }
         }
 
-//        private static List<string> parserXML(XmlDocument xmlDocument, string nameXmlNode, string nameAtribute)
-//        {
-//            XmlNodeList nodes =
-//                xmlDocument.DocumentElement.SelectNodes("/weatherdata/product/time/location/" + nameXmlNode);
-//            List<string> values = new List<string>();
-//            Console.WriteLine("nodes: " + nodes.Count);
-//            foreach (XmlNode node in nodes)
-//            {
-//                values.Add(node.Attributes[nameAtribute].Value);
-//            }
-//
-//            return values;
-//        }
+        //        private static List<string> parserXML(XmlDocument xmlDocument, string nameXmlNode, string nameAtribute)
+        //        {
+        //            XmlNodeList nodes =
+        //                xmlDocument.DocumentElement.SelectNodes("/weatherdata/product/time/location/" + nameXmlNode);
+        //            List<string> values = new List<string>();
+        //            Console.WriteLine("nodes: " + nodes.Count);
+        //            foreach (XmlNode node in nodes)
+        //            {
+        //                values.Add(node.Attributes[nameAtribute].Value);
+        //            }
+        //
+        //            return values;
+        //        }
     }
 }
