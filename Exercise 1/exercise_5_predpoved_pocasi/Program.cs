ebfd8d3ca583806bd3c215bd7a7b9fa053738b7f
pavelkrivda <st52934@student.upce.cz>
2019-03-09 23:34:08 +0100

    Aktualizace projektů

--- Exercise 1/exercise_5_predpoved_pocasi/Program.cs
+++ Exercise 1/exercise_5_predpoved_pocasi/Program.cs
@@ -1,35 +1,33 @@
-﻿using exercis_5_predpoved_pocasi;
-using exercise_5_predpoved_pocasi.Yandex;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Net;
 using System.Xml;
+using exercise_5_predpoved_pocasi.Yandex;
 
 namespace exercis_1_5_predpoved_pocasi
 {
     // Ke geolokaci bylo využito řešení z https://github.com/Verhov/YandexGeocoder
 
-    class Program
+    internal class Program
     {
-        private static List<string> teploty = new List<string>();
-        private static List<string> rychlostVetru = new List<string>();
-        private static List<string> tlak = new List<string>();
-        private static List<string> cas = new List<string>();
+        private static readonly List<string> teploty = new List<string>();
+        private static readonly List<string> rychlostVetru = new List<string>();
+        private static readonly List<string> tlak = new List<string>();
+        private static readonly List<string> cas = new List<string>();
 
         public static void Main(string[] args)
         {
-            WebClient client = new WebClient();
-            XmlDocument xmlDocument = new XmlDocument();
+            var client = new WebClient();
+            var xmlDocument = new XmlDocument();
 
             //string result =
             //    client.DownloadString("https://api.met.no/weatherapi/locationforecast/1.9/?lat=60.10&lon=9.58");
 
-            string location = nactiLokaci();
-            string lat, lon;
-            zjistiPolohu(location, out lat, out lon);
+            var location = nactiLokaci();
+            zjistiPolohu(location, out var lat, out var lon);
 
-            string result = client.DownloadString(
-                    string.Format("https://api.met.no/weatherapi/locationforecast/1.9/?lat={0}&lon={1}", lat, lon));
+            var result = client.DownloadString(
+                $"https://api.met.no/weatherapi/locationforecast/1.9/?lat={lat}&lon={lon}");
 
             xmlDocument.LoadXml(result);
             xmlParsovaniRekurzivne(xmlDocument);
@@ -39,13 +37,13 @@ namespace exercis_1_5_predpoved_pocasi
         private static string nactiLokaci()
         {
             Console.Write("Zadej místo pro které se zobrazí informace o počasí: ");
-            string location = Console.ReadLine();
+            var location = Console.ReadLine();
             return location;
         }
 
         private static void zjistiPolohu(string location, out string lat, out string lon)
         {
-            GeoObjectCollection results = YandexGeocoder.Geocode(location);
+            var results = YandexGeocoder.Geocode(location);
             lat = results[0].Point.Lat.ToString().Replace(',', '.');
             lon = results[0].Point.Long.ToString().Replace(',', '.');
             Console.WriteLine($"Souřadnice zadaného místa.\nZeměpisná šířka: {lat}  zeměpisná délka: {lon}");
@@ -71,22 +69,14 @@ namespace exercis_1_5_predpoved_pocasi
         private static void hledanyUdaj(XmlNode hlavniNod, XmlNode podNod)
         {
             if (hlavniNod.Name.Equals("product"))
-            {
                 zpracujCas(podNod);
-            }
-            else if (hlavniNod.Name.Equals("location"))
-            {
-                zpracujUdaj(podNod);
-            }
+            else if (hlavniNod.Name.Equals("location")) zpracujUdaj(podNod);
         }
 
         private static void zpracujCas(XmlNode podNod)
         {
-            string value = podNod.Attributes["to"].Value;
-            if (cas.Count == 0 || !cas.Contains(value))
-            {
-                cas.Add(value);
-            }
+            var value = podNod.Attributes["to"].Value;
+            if (cas.Count == 0 || !cas.Contains(value)) cas.Add(value);
         }
 
         private static void zpracujUdaj(XmlNode podNod)
@@ -107,36 +97,32 @@ namespace exercis_1_5_predpoved_pocasi
 
         private static void vypisVysledku()
         {
-            for (int i = 0; i < tlak.Count; i++)
-            {
+            for (var i = 0; i < tlak.Count; i++)
                 Console.WriteLine($"{cas[i]}\t {teploty[i]}°C\t {rychlostVetru[i]}m/s\t {tlak[i]}hPa");
-            }
         }
 
         private static void xmlParsovaniJmenoNode(XmlDocument xmlDocument)
         {
             Console.WriteLine("Po nodech");
-            List<string> teploty = parserXML(xmlDocument, "temperature", "value");
-            List<string> rychlostVetru = parserXML(xmlDocument, "windSpeed", "mps");
-            List<string> tlak = parserXML(xmlDocument, "pressure", "value");
-            List<string> cas = new List<string>();
+            var teploty = parserXML(xmlDocument, "temperature", "value");
+            var rychlostVetru = parserXML(xmlDocument, "windSpeed", "mps");
+            var tlak = parserXML(xmlDocument, "pressure", "value");
 
-            for (int i = 0; i < tlak.Count; i++)
-            {
+            for (var i = 0; i < tlak.Count; i++)
                 Console.WriteLine($"{teploty[i]}°C\t {rychlostVetru[i]}m/s\t {tlak[i]}hPa");
-            }
         }
 
         private static List<string> parserXML(XmlDocument xmlDocument, string nameXmlNode, string nameAtribute)
         {
-            XmlNodeList nodes =
+            var nodes =
                 xmlDocument.DocumentElement.SelectNodes("/weatherdata/product/time/location/" + nameXmlNode);
-            List<string> values = new List<string>();
+            var values = new List<string>();
+
+            if (nodes == null) return values;
             Console.WriteLine("nodes: " + nodes.Count);
             foreach (XmlNode node in nodes)
-            {
-                values.Add(node.Attributes[nameAtribute].Value);
-            }
+                if (node.Attributes != null)
+                    values.Add(node.Attributes[nameAtribute].Value);
 
             return values;
         }
